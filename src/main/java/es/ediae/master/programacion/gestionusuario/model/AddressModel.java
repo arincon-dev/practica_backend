b3660679799a283088e9a40f9f0ffe59e03236ff
AlvaroRinki <164490545+AlvaroRinki@users.noreply.github.com>
2026-05-04 20:28:38 +0200

    feat: create AddressModel class for address management

---
+++ src/main/java/es/ediae/master/programacion/gestionusuario/model/AddressModel.java
@@ -0,0 +1,58 @@
+package es.ediae.master.programacion.gestionusuario.model;
+
+public class AddressModel {
+
+    private Integer id;
+    private String streetName;
+    private Integer streetNumber;
+    private Boolean mainAddress;
+    private Integer userId;
+
+    public AddressModel(Integer id, String streetName, Integer streetNumber, Boolean mainAddress, Integer userId) {
+        this.id = id;
+        this.streetName = streetName;
+        this.streetNumber = streetNumber;
+        this.mainAddress = mainAddress;
+        this.userId = userId;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getStreetName() {
+        return streetName;
+    }
+
+    public void setStreetName(String streetName) {
+        this.streetName = streetName;
+    }
+
+    public Integer getStreetNumber() {
+        return streetNumber;
+    }
+
+    public void setStreetNumber(Integer streetNumber) {
+        this.streetNumber = streetNumber;
+    }
+
+    public Boolean getMainAddress() {
+        return mainAddress;
+    }
+
+    public void setMainAddress(Boolean mainAddress) {
+        this.mainAddress = mainAddress;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+}
