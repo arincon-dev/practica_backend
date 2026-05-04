f5d87c0c0720191d433a7874d064ca7767cbe4b4
AlvaroRinki <164490545+AlvaroRinki@users.noreply.github.com>
2026-05-04 20:29:06 +0200

    feat: create UserModel class for user representation

---
+++ src/main/java/es/ediae/master/programacion/gestionusuario/model/UserModel.java
@@ -0,0 +1,131 @@
+package es.ediae.master.programacion.gestionusuario.model;
+
+import java.sql.Time;
+import java.util.Date;
+
+public class UserModel {
+
+    private Integer id;
+    private String username;
+    private String password;
+    private Date createdAt;
+    private String name;
+    private String firstSurname;
+    private String secondSurname;
+    private Date birthdate;
+    private Time breakfastTime;
+    private Boolean isAdmin;
+    private GenderModel gender;
+    private JobTitleModel jobTitle;
+
+    public UserModel(Integer id, String username, String password, Date createdAt, String name, String firstSurname, String secondSurname, Date birthdate, Time breakfastTime, Boolean isAdmin, GenderModel gender, JobTitleModel jobTitle) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.createdAt = createdAt;
+        this.name = name;
+        this.firstSurname = firstSurname;
+        this.secondSurname = secondSurname;
+        this.birthdate = birthdate;
+        this.breakfastTime = breakfastTime;
+        this.isAdmin = isAdmin;
+        this.gender = gender;
+        this.jobTitle = jobTitle;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFirstSurname() {
+        return firstSurname;
+    }
+
+    public void setFirstSurname(String firstSurname) {
+        this.firstSurname = firstSurname;
+    }
+
+    public String getSecondSurname() {
+        return secondSurname;
+    }
+
+    public void setSecondSurname(String secondSurname) {
+        this.secondSurname = secondSurname;
+    }
+
+    public Date getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(Date birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public Time getBreakfastTime() {
+        return breakfastTime;
+    }
+
+    public void setBreakfastTime(Time breakfastTime) {
+        this.breakfastTime = breakfastTime;
+    }
+
+    public Boolean getIsAdmin() {
+        return isAdmin;
+    }
+
+    public void setIsAdmin(Boolean isAdmin) {
+        this.isAdmin = isAdmin;
+    }
+
+    public GenderModel getGender() {
+        return gender;
+    }
+
+    public void setGender(GenderModel gender) {
+        this.gender = gender;
+    }
+
+    public JobTitleModel getJobTitle() {
+        return jobTitle;
+    }
+
+    public void setJobTitle(JobTitleModel jobTitle) {
+        this.jobTitle = jobTitle;
+    }
+}
