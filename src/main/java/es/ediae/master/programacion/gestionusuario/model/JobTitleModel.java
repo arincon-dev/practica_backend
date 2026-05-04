59b99e70b67d4bcede474d4d6a899b430b4aae8c
AlvaroRinki <164490545+AlvaroRinki@users.noreply.github.com>
2026-05-04 20:28:59 +0200

    feat: create JobTitleModel class for job title representation

---
+++ src/main/java/es/ediae/master/programacion/gestionusuario/model/JobTitleModel.java
@@ -0,0 +1,28 @@
+package es.ediae.master.programacion.gestionusuario.model;
+
+public class JobTitleModel {
+
+    private Integer id;
+    private String name;
+
+    public JobTitleModel(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
