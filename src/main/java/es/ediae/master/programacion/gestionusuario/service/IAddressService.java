87d0fa6985c5f61d634896446773341e4e00f0af
AlvaroRinki <164490545+AlvaroRinki@users.noreply.github.com>
2026-05-05 19:13:33 +0200

    feat(IAddressService): rename methods for consistency and clarity

--- src/main/java/es/ediae/master/programacion/gestionusuario/service/IAddressService.java
+++ src/main/java/es/ediae/master/programacion/gestionusuario/service/IAddressService.java
@@ -5,10 +5,10 @@ import java.util.List;
 import es.ediae.master.programacion.gestionusuario.model.AddressModel;
 
 public interface IAddressService {
-
+    
     List<AddressModel> obtenerDirecciones(Integer userId, String nickUsuario, String nickContrasena);
-    AddressModel obtenerDireccionPorId(Integer id, String nickUsuario, String nickContrasena);
-    AddressModel crearDireccion(AddressModel addressModel, String nickUsuario, String nickContrasena);
-    AddressModel actualizarDireccion(Integer id, AddressModel addressModel, String nickUsuario, String nickContrasena);
+    AddressModel obtenerDireccion(Integer id, String nickUsuario, String nickContrasena);
+    AddressModel crearDireccion(AddressModel model, String nickUsuario, String nickContrasena);
+    AddressModel actualizarDireccion(Integer id, AddressModel model, String nickUsuario, String nickContrasena);
     Boolean eliminarDireccion(Integer id, String nickUsuario, String nickContrasena);
 }
