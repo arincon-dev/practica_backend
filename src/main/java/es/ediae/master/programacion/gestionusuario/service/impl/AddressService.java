f4da33258668934457dfb759362d735ed0384349
AlvaroRinki <164490545+AlvaroRinki@users.noreply.github.com>
2026-05-05 18:44:10 +0200

    feat(AddressService): refactor methods to use AddressModel and add authentication check

--- src/main/java/es/ediae/master/programacion/gestionusuario/service/impl/AddressService.java
+++ src/main/java/es/ediae/master/programacion/gestionusuario/service/impl/AddressService.java
@@ -2,44 +2,67 @@ package es.ediae.master.programacion.gestionusuario.service.impl;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import es.ediae.master.programacion.gestionusuario.entity.AddressEntity;
+import es.ediae.master.programacion.gestionusuario.entity.UserEntity;
+import es.ediae.master.programacion.gestionusuario.model.AddressModel;
 import es.ediae.master.programacion.gestionusuario.repository.AddressRepository;
+import es.ediae.master.programacion.gestionusuario.repository.UserRepository;
 import es.ediae.master.programacion.gestionusuario.service.IAddressService;
 
 @Service
 public class AddressService implements IAddressService {
 
-    @Autowired
-    private AddressRepository addressRepository;
+    private final UserRepository userRepository;
+    private final AddressRepository addressRepository;
 
-    @Override
-    public List<AddressEntity> getAllAddresses() {
-        return addressRepository.findAll();
+    public AddressService(UserRepository userRepository, AddressRepository addressRepository) {
+        this.userRepository = userRepository;
+        this.addressRepository = addressRepository;
+    }
+
+    public boolean authenticate(String nickUsuario, String nickContrasena) {
+        return userRepository.existsByUsernameAndPassword(nickUsuario, nickContrasena);
     }
 
     @Override
-    public AddressEntity getAddressById(Integer id) {
-        return addressRepository.findById(id).orElse(null);
+    public List<AddressModel> obtenerDirecciones(Integer userId, String nickUsuario, String nickContrasena) {
+        if (!authenticate(nickUsuario, nickContrasena)) return null;
+        return addressRepository.findByUserId(userId).stream().map(this::toModel).toList();
     }
 
     @Override
-    public AddressEntity createAddress(AddressEntity address) {
-        return addressRepository.save(address);
+    public AddressModel obtenerDireccionPorId(Integer id, String nickUsuario, String nickContrasena) {
+        throw new UnsupportedOperationException("Not supported yet.");
     }
 
     @Override
-    public AddressEntity updateAddress(Integer id, AddressEntity address) {
-        if (addressRepository.existsById(id)) {
-            return addressRepository.save(address);
-        }
-        return null;
+    public AddressModel crearDireccion(AddressModel addressModel, String nickUsuario, String nickContrasena) {
+        throw new UnsupportedOperationException("Not supported yet.");
     }
 
     @Override
-    public void deleteAddress(Integer id) {
-        addressRepository.deleteById(id);
+    public AddressModel actualizarDireccion(Integer id, AddressModel addressModel, String nickUsuario, String nickContrasena) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public Boolean eliminarDireccion(Integer id, String nickUsuario, String nickContrasena) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    private AddressModel toModel(AddressEntity e) {
+    return new AddressModel(e.getId(), e.getStreetName(), e.getStreetNumber(),
+        e.getMainAddress(), e.getUser().getId());
+    }
+
+    private AddressEntity toEntity(AddressModel m, UserEntity user) {
+        AddressEntity e = new AddressEntity();
+        e.setStreetName(m.getStreetName());
+        e.setStreetNumber(m.getStreetNumber());
+        e.setMainAddress(m.getMainAddress());
+        e.setUser(user);
+        return e;
     }
 }
