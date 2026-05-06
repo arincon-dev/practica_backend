42c196394db8f3b123ca79fb367b86b32b02d22e
AlvaroRinki <164490545+AlvaroRinki@users.noreply.github.com>
2026-05-06 20:13:58 +0200

    feat: comment out unused methods and remove FakeServiceImpl

--- src/main/java/es/ediae/master/programacion/gestionusuario/controller/AddressController.java
+++ src/main/java/es/ediae/master/programacion/gestionusuario/controller/AddressController.java
@@ -1,21 +1,13 @@
 package es.ediae.master.programacion.gestionusuario.controller;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import es.ediae.master.programacion.gestionusuario.entity.AddressEntity;
-import es.ediae.master.programacion.gestionusuario.service.impl.AddressService;
 
 
 @RestController
 @RequestMapping("/api/v1")
 public class AddressController {
-
+    /*
     @Autowired
     private AddressService addressService;
 
@@ -32,5 +24,5 @@ public class AddressController {
     @GetMapping("/address/{id}")
     public AddressEntity getAddressById(@PathVariable Integer id) {
         return addressService.getAddressById(id);
-    }
+    }*/
 }
