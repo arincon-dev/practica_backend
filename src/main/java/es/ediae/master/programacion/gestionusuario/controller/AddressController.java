90c8be97e8ae8bdb79092a53d505bcd3412b094d
arincon-dev <alvaro.rinki@gmail.com>
2026-05-10 17:20:30 +0200

    feat: implement UserController with user management endpoints and remove unused controllers

--- src/main/java/es/ediae/master/programacion/gestionusuario/controller/AddressController.java
+++ src/main/java/es/ediae/master/programacion/gestionusuario/controller/AddressController.java
@@ -1,28 +1,77 @@
 package es.ediae.master.programacion.gestionusuario.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import es.ediae.master.programacion.gestionusuario.model.AddressModel;
+import es.ediae.master.programacion.gestionusuario.service.IAddressService;
 
 
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping("/api/v1/direcciones")
 public class AddressController {
-    /*
-    @Autowired
-    private AddressService addressService;
-
-    @GetMapping("/user/{userId}/addresses")
-    public List<AddressEntity> getAddressesByUser(@PathVariable Integer userId) {
-        return addressService.findByUserId(userId);
-    }
-
-    @GetMapping("/addresses")
-    public List<AddressEntity> getAllAddress() {
-        return addressService.getAllAddresses();
-    }
     
-    @GetMapping("/address/{id}")
-    public AddressEntity getAddressById(@PathVariable Integer id) {
-        return addressService.getAddressById(id);
-    }*/
+    @Autowired
+    private IAddressService addressService;
+
+    @GetMapping("/usuario/{userId}")
+    public ModelMap obtenerDirecciones(@PathVariable("userId") Integer userId, @RequestParam String nickUsuario, @RequestParam String nickContrasena) {
+        try {
+            List<AddressModel> result = addressService.obtenerDirecciones(userId, nickUsuario, nickContrasena);
+            return GeneralControllerUtils.crearRespuestaModelMapOk(result);
+        } catch (Exception e) {
+            return GeneralControllerUtils.crearRespuestaModelMapError(e);
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ModelMap obtenerDireccion(@PathVariable("id") Integer id, @RequestParam String nickUsuario, @RequestParam String nickContrasena) {
+        try {
+            AddressModel result = addressService.obtenerDireccion(id, nickUsuario, nickContrasena);
+            return GeneralControllerUtils.crearRespuestaModelMapOk(result);
+        } catch (Exception e) {
+            return GeneralControllerUtils.crearRespuestaModelMapError(e);
+        }
+    }
+
+    @PostMapping("/")
+    public ModelMap crearDireccion(@RequestBody AddressModel addressModel, @RequestParam String nickUsuario, @RequestParam String nickContrasena) {
+        try {
+            AddressModel result = addressService.crearDireccion(addressModel, nickUsuario, nickContrasena);
+            return GeneralControllerUtils.crearRespuestaModelMapOk(result);
+        } catch (Exception e) {
+            return GeneralControllerUtils.crearRespuestaModelMapError(e);
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ModelMap actualizarDireccion(@PathVariable("id") Integer id, @RequestBody AddressModel addressModel, @RequestParam String nickUsuario, @RequestParam String nickContrasena) {
+        try {
+            AddressModel result = addressService.actualizarDireccion(id, addressModel, nickUsuario, nickContrasena);
+            return GeneralControllerUtils.crearRespuestaModelMapOk(result);
+        } catch (Exception e) {
+            return GeneralControllerUtils.crearRespuestaModelMapError(e);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ModelMap eliminarDireccion(@PathVariable("id") Integer id, @RequestParam String nickUsuario, @RequestParam String nickContrasena) {
+        try {
+            Boolean result = addressService.eliminarDireccion(id, nickUsuario, nickContrasena);
+            return GeneralControllerUtils.crearRespuestaModelMapOk(result);
+        } catch (Exception e) {
+            return GeneralControllerUtils.crearRespuestaModelMapError(e);
+        }
+    }
 }
---
+++ src/main/java/es/ediae/master/programacion/gestionusuario/controller/UserController.java
@@ -0,0 +1,116 @@
+package es.ediae.master.programacion.gestionusuario.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import es.ediae.master.programacion.gestionusuario.model.GenderModel;
+import es.ediae.master.programacion.gestionusuario.model.JobTitleModel;
+import es.ediae.master.programacion.gestionusuario.model.UserModel;
+import es.ediae.master.programacion.gestionusuario.service.IGenderService;
+import es.ediae.master.programacion.gestionusuario.service.IJobTitleService;
+import es.ediae.master.programacion.gestionusuario.service.IUserService;
+
+
+@RestController
+@RequestMapping("/api/v1/usuarios")
+public class UserController {
+
+    @Autowired
+    private IUserService userService;
+    @Autowired
+    private IGenderService genderService;
+    @Autowired
+    private IJobTitleService jobTitleService;
+
+    @PostMapping("/iniciar-sesion")
+    public ModelMap iniciarSesion(@RequestParam String username, @RequestParam String password) {
+        try {
+            boolean result = userService.iniciarSesion(username, password);
+            return GeneralControllerUtils.crearRespuestaModelMapOk(result);
+        } catch (Exception e) {
+            return GeneralControllerUtils.crearRespuestaModelMapError(e);
+        }
+    }
+    @GetMapping("/")
+    public ModelMap obtenerUsuarios(@RequestParam String nickUsuario, @RequestParam String nickContrasena) {
+        try {
+            List<UserModel> result = userService.obtenerUsuarios(nickUsuario, nickContrasena);
+            return GeneralControllerUtils.crearRespuestaModelMapOk(result);
+        } catch (Exception e) {
+            return GeneralControllerUtils.crearRespuestaModelMapError(e);
+        }
+    }
+    
+    @GetMapping("/{id}")
+    public ModelMap obtenerUsuario(@PathVariable("id") Integer id, @RequestParam String nickUsuario, @RequestParam String nickContrasena) {
+        try {
+            UserModel result = userService.obtenerUsuarioPorId(id, nickUsuario, nickContrasena);
+            return GeneralControllerUtils.crearRespuestaModelMapOk(result);
+        } catch (Exception e) {
+            return GeneralControllerUtils.crearRespuestaModelMapError(e);
+        }
+    }
+    
+    @PostMapping("/")
+    public ModelMap crearUsuario(@RequestBody UserModel userModel, @RequestParam String nickUsuario, @RequestParam String nickContrasena) {
+        try {
+            UserModel result = userService.crearUsuario(userModel, nickUsuario, nickContrasena);
+            return GeneralControllerUtils.crearRespuestaModelMapOk(result);
+        } catch (Exception e) {
+            return GeneralControllerUtils.crearRespuestaModelMapError(e);
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ModelMap actualizarUsuario(@PathVariable("id") Integer id, @RequestBody UserModel userModel, @RequestParam String nickUsuario, @RequestParam String nickContrasena) {
+        try {
+            UserModel result = userService.actualizarUsuario(id, userModel, nickUsuario, nickContrasena);
+            return GeneralControllerUtils.crearRespuestaModelMapOk(result);
+        } catch (Exception e) {
+            return GeneralControllerUtils.crearRespuestaModelMapError(e);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ModelMap eliminarUsuario(@PathVariable("id") Integer id, @RequestParam String nickUsuario, @RequestParam String nickContrasena) {
+        try {
+            boolean result = userService.eliminarUsuario(id, nickUsuario, nickContrasena);
+            return GeneralControllerUtils.crearRespuestaModelMapOk(result);
+        } catch (Exception e) {
+            return GeneralControllerUtils.crearRespuestaModelMapError(e);
+        }
+    }
+
+    // GenderController
+    @GetMapping("/generos")
+    public ModelMap obtenerGeneros(@RequestParam String nickUsuario, @RequestParam String nickContrasena) {
+        try {
+            List<GenderModel> result = genderService.obtenerGeneros(nickUsuario, nickContrasena);
+            return GeneralControllerUtils.crearRespuestaModelMapOk(result);
+        } catch (Exception e) {
+            return GeneralControllerUtils.crearRespuestaModelMapError(e);
+        }
+    }
+
+    // JobTitleController
+    @GetMapping("/puestos-trabajo")
+    public ModelMap obtenerPuestosTrabajo(@RequestParam String nickUsuario, @RequestParam String nickContrasena) {
+        try {
+            List<JobTitleModel> result = jobTitleService.obtenerPuestosTrabajo(nickUsuario, nickContrasena);
+            return GeneralControllerUtils.crearRespuestaModelMapOk(result);
+        } catch (Exception e) {
+            return GeneralControllerUtils.crearRespuestaModelMapError(e);
+        }
+    }
+}
