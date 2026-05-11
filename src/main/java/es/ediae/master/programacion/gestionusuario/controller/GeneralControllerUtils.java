f9e086b65574314963cfc9a0b2200757072301e1
AlvaroRinki <164490545+AlvaroRinki@users.noreply.github.com>
2026-05-11 17:13:38 +0200

    feat: change access modifiers of crearRespuestaModelMapOk and crearRespuestaModelMapError methods to public

--- src/main/java/es/ediae/master/programacion/gestionusuario/controller/GeneralControllerUtils.java
+++ src/main/java/es/ediae/master/programacion/gestionusuario/controller/GeneralControllerUtils.java
@@ -4,7 +4,7 @@ import es.ediae.master.programacion.gestionusuario.constant.GeneralConstant;
 import org.springframework.ui.ModelMap;
 
 public class GeneralControllerUtils {
-    static ModelMap crearRespuestaModelMapOk(Object data) {
+    public static ModelMap crearRespuestaModelMapOk(Object data) {
         ModelMap response = new ModelMap();
         response.put(GeneralConstant.TYPE, GeneralConstant.OK);
         response.put(GeneralConstant.EXCEPTION, null);
@@ -12,7 +12,7 @@ public class GeneralControllerUtils {
         return response;
     }
 
-    static ModelMap crearRespuestaModelMapError(Exception ex) {
+    public static ModelMap crearRespuestaModelMapError(Exception ex) {
         ModelMap response = new ModelMap();
         response.put(GeneralConstant.TYPE, GeneralConstant.EXCEPTION);
         response.put(GeneralConstant.EXCEPTION, ex);
